aef86a170ff5ba0d6ada99c2bd2d702ddb212360
Fosterosei07 <oprempehfoster@gmail.com>
2023-10-01 20:39:07 +0000

    UPDATE: Modifying existing files

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const expressLayouts = require('express-ejs-layouts')
+const routes = require('./routes/userRoute')
+const ejs = require('ejs')
 
 dotenv.config()
 
@@ -8,12 +11,22 @@ const port = process.env.port || 7007
 const app = express()
 
 
+app.set('view engine','ejs');
+app.use(expressLayouts);
 
+
+//middleware
+app.use('/', userRoute);
+app.use(express.static('public'))
 
 app.get('/', (req, res) =>{
     res.send('GOD IS THE GREATEST')
 })
 
 app.listen(port, () => {
+    try{
     console.log(`Server is connected on http://localhost:${port}`)
+    }catch(error){
+        console.log(error)
+    }
 })
---
+++ routes/userRoute.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const {home} = require('../controller/userController');
+
+const app = express.Router()
+
+app.get('/',home)
+
+
+module.exports=app
